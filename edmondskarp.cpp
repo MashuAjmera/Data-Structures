10705854007309a26ec8301338662dd6c4f4dbb4
Mashu Ajmera <mashuajmera@gmail.com>
2020-11-10 12:25:40 +0530

    edmonds karp implemented

--- edmondskarp.cpp
+++ edmondskarp.cpp
@@ -1,63 +1,74 @@
-#include <iostream>
-#include <climits> // for INT_MAX
-#include <queue>   // for queues
+// Program Author: Mashu Ajmera
+
+#include <iostream> // for i/o
+#include <climits>  // for INT_MAX
+#include <queue>    // for queues
 using namespace std;
 const int V = 6; // Number of vertices in the graph
 
-bool bfs(int rgraph[][V], int s, int t, int parent[])
+bool bfs(int rgraph[][V], int s, int t, int parent[]) // Breadth First Search (BFS) to find the shortest path from s to t
 {
-    bool visited[V] = {false};
+    bool visited[V] = {false}; // we save whether the junction has been visited
     queue<int> q;
-    q.push(s);
-    visited[s] = true;
-    parent[s] = -1;
-    while (!q.empty())
+    visited[s] = true; // we start by visiting the source
+    q.push(s);         // we start by pushing q in the queue
+    parent[s] = -1;    // we make the parent of the source as -1
+    while (!q.empty()) // untill there is no junction left to be explored in the queue
     {
         int n = q.front();
         for (int i = 0; i < V; i++)
-        {
-            if (!visited[i] && rgraph[n][i] > 0)
+            if (!visited[i] && rgraph[n][i] > 0) // if the junction has not been visited and there is some residual capacity in the pipe
             {
-                q.push(i);
-                visited[i] = true;
+                q.push(i);         // we add the junction in the queue
+                visited[i] = true; // mark the junction as visited
                 parent[i] = n;
             }
-        }
-        q.pop();
+
+        q.pop(); // we pop the explored junction out
     }
-    return visited[t];
+    return visited[t]; // we return wheter we were label to reach terminal as we start from s
 }
 
 int edmondsKarp(int graph[][V], int s, int t)
 {
-    int maxFlow = 0, parent[V], rgraph[V][V], min;
+    int maxFlow = 0, parent[V], rgraph[V][V], min; // we save the shortest path in the parent array, we save the residual capacity in the rgraph array
 
     for (int i = 0; i < V; i++)
         for (int j = 0; j < V; j++)
-            rgraph[i][j] = graph[i][j];
+            rgraph[i][j] = graph[i][j]; // we initialize the residual capacity as the original capacity of the pipes
 
-    while (bfs(rgraph, s, t, parent))
+    while (bfs(rgraph, s, t, parent)) // we check if a path exists from s to t, if so then we know it is saved in the parents aarray
     {
         min = INT_MAX;
-        for (int i = t; i != -1; i = parent[i])
-        {
+        for (int i = t; i != -1; i = parent[i]) // we calculate the pipe with the minimum capacity in our shortest path
             if (min > rgraph[parent[i]][i])
-            {
                 min = rgraph[parent[i]][i];
-            }
-        }
-        for (int i = t; i != -1; i = parent[i])
+
+        for (int i = t; i != -1; i = parent[i]) // we now since all the pipes in the shortest path will be filled with min, we decrease their residual capacities by min
         {
             rgraph[parent[i]][i] -= min;
-            // rgraph[i][parent[i]] += min;
+            rgraph[i][parent[i]] += min;
         }
-        maxFlow += min;
+        maxFlow += min; // we add all the flows to calculate our maximum flow
     }
-    return maxFlow;
+    return maxFlow; //  we return the maximum flow calculated
 }
 
-int main()
+int main() // the driver function for our implementation of Edmonds Karp Algorithm
 {
+    int graph[V][V], s, t;
+
+    cout << "Enter the graph matrix of " << V << " vertices:\n";
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            cin >>
+                graph[i][j];
+
+    cout << "Enter the source node: ";
+    cin >> s;
+    cout << "Enter the terminal node: ";
+    cin >> t;
+
     // Test Case-1: V=6, s=0,t=5, ans=10
     // int graph[V][V] = {{0, 7, 4, 0, 0, 0},
     //                    {0, 0, 0, 5, 3, 0},
@@ -79,15 +90,13 @@ int main()
     //                    {0, 0, 0, 0}};
 
     // Test Case-4: V=6, s=0,t=5, ans=23
-    int graph[V][V] = {{0, 16, 13, 0, 0, 0},
-                       {0, 0, 10, 12, 0, 0},
-                       {0, 4, 0, 0, 14, 0},
-                       {0, 0, 9, 0, 0, 20},
-                       {0, 0, 0, 7, 0, 4},
-                       {0, 0, 0, 0, 0, 0}};
+    // int graph[V][V] = {{0, 16, 13, 0, 0, 0},
+    //                    {0, 0, 10, 12, 0, 0},
+    //                    {0, 4, 0, 0, 14, 0},
+    //                    {0, 0, 9, 0, 0, 20},
+    //                    {0, 0, 0, 7, 0, 4},
+    //                    {0, 0, 0, 0, 0, 0}};
 
-    int s = 0, t = 5;
-    cout << "Maximum Flow possible is: " << edmondsKarp(graph, s, t);
-
+    cout << "Maximum flow possible is: " << edmondsKarp(graph, s, t);
     return 0;
 }
